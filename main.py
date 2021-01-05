f203275d9dbf32ca68da8c336a3d520cc03db9fe
sBakerMadeira <76955595+sBakerMadeira@users.noreply.github.com>
2021-01-05 10:09:34 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+
+    # statement 1
+    user_name = input("what is your name?")
+    # statement 2
+    user_age = input("how old are you?")
+    # statement 3
+    user_favorite_color = input("what is your favorite color?")
+    # statement 4
+    user_home = input("where are you from?")
+    # statement 5
+    user_pets = input("how many pets do you have?")
